9ae192c1ebc27ba12f9aa6656133ad48b8f4885a
706ash <114875724+706ash@users.noreply.github.com>
2024-02-14 21:22:20 +0530

    learn django

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm' : searchTerm})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to about page')
+
